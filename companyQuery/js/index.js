570f811e4b91c31627ea3be76474de402b066305
LWJ <834672602@qq.com>
2019-04-21 16:30:57 +0800

    modify

--- companyQuery/js/index.js
+++ companyQuery/js/index.js
@@ -1,6 +1,12 @@
+
 var mySwiper = new Swiper('.swiper-container', {
     autoplay: 4000,//可选选项，自动滑动
     loop:true,
     pagination : '.swiper-pagination',
-    
-})
+});
+
+$('.tab-list>.list-item').mouseenter(function(){
+    $(this).addClass('selected').siblings('li').removeClass('selected');
+    var index = $(this).index();
+    $('.search-tabs>.tab-inforArea').eq(index).removeClass('hide').siblings('.tab-inforArea').addClass('hide');
+});
