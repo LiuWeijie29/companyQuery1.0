68f640fcbb68c5892798c19017bf067456f43535
LiuWeijie29 <44127644+LiuWeijie29@users.noreply.github.com>
2019-04-30 10:32:25 +0800

    2019/4/30
    update

--- companyQuery/js/index.js
+++ companyQuery/js/index.js
@@ -1,12 +1,28 @@
 
-var mySwiper = new Swiper('.swiper-container', {
-    autoplay: 4000,//可选选项，自动滑动
-    loop:true,
-    pagination : '.swiper-pagination',
-});
-
+//tab栏切换
 $('.tab-list>.list-item').mouseenter(function(){
     $(this).addClass('selected').siblings('li').removeClass('selected');
     var index = $(this).index();
     $('.search-tabs>.tab-inforArea').eq(index).removeClass('hide').siblings('.tab-inforArea').addClass('hide');
 });
+
+//主页面搜索标签类切换
+$('.searchLi>li').mouseenter(function(){
+    $(this).addClass('seachSelected').siblings('li').removeClass('seachSelected');
+    var index = $(this).index();
+    var searchIpt = $('#search-ipt');
+    switch(index){
+        case 0:searchIpt.attr('placeholder','请输入企业名称，如腾讯科技有限公司');
+        break;
+        case 1:searchIpt.attr('placeholder','请输入企业注册号');
+        break;
+        case 2:searchIpt.attr('placeholder','请输入法人或股东的姓名');
+        break;
+        case 3:searchIpt.attr('placeholder','请输入企业地址');
+        break;
+        case 4:searchIpt.attr('placeholder','请输入企业电话或法人电话');
+        break;
+    }
+});
+
+
