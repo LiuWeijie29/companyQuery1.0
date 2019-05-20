1eb8761ed98a6954f43913eb3fc4a36922d7f6ea
LiuWeijie29 <44127644+LiuWeijie29@users.noreply.github.com>
2019-05-20 15:38:20 +0800

    new change
    优化请求

--- companyQuery/js/detailInfor.js
+++ companyQuery/js/detailInfor.js
@@ -1,4 +1,5 @@
 var company = {};
+var mychart;
 $(function(){
     var id = $.query.get("id");
     var url = "http://106.14.151.119:3000/api/findOne?id="+id;
@@ -102,27 +103,48 @@ $('#boxTabs>ul>li').eq(1).on('click',function(){
 
 //企业图谱
 function dataViewShow(){
-    var mychart = echarts.init(document.getElementById('dataView'));
+    mychart = echarts.init(document.getElementById('dataView')); //mychart是全局变量
     // 指定图表的配置项和数据
     option = {
         title: {
             text: '企业信息图谱'
         },
-        tooltip: {},
+        tooltip:{
+            formatter:function(e){
+                if(e.name == ''){
+                    return '暂无'
+                }
+                return e.value + ": " + e.name;
+            }
+        },
+        toolbox:{
+            show : true,
+            feature : {//启用功能
+                dataView: {show:true},//数据窗口
+                restore : {show: true},//restore，还原，复位原始图表
+            }
+        },
         animationDurationUpdate: 1500,
         animationEasingUpdate: 'quinticInOut',
         series : [
             {
-                name:'基本信息',
                 type: 'graph',
+                // layout: 'force',
                 layout: 'circular',
                 symbolSize: 90,
-                roam: true,
-                focusNodeAdjacency:true,
+                roam: 'true',
+                // force: {
+                //     edgeLength: [100,200],//线的长度，这个距离也会受 repulsion，支持设置成数组表达边长的范围
+                //     repulsion: 100//节点之间的斥力因子。值越大则斥力越大
+                // },
+                // draggable: true,//指示节点是否可以拖动
                 label: {
                     normal: {
                         show: true,
-                        formatter:'{c}'       //标签内容格式器。模板变量有 {a}、{b}、{c}，分别表示系列名，数据名，数据值。
+                        // formatter:function(e){
+                        //     return e.value+": "+e.name
+                        // }       //标签内容格式器。模板变量有 {a}、{b}、{c}，分别表示系列名，数据名，数据值。
+                        formatter:'{c}'
                     }
                 },
 
@@ -238,113 +260,307 @@ function dataViewShow(){
             }
         ]
     };
-    
+    // //点击跳转
+    mychart.on('click',function(e){
+        // console.log(e.value); 
+        //如果点击了企业法人，那么搜索该企业法人的数据并且返回新图谱   
+        if(e.value == '企业法人'){
+            clickLegalPerson(e)
+        }
+        //if点击了其他
+        if(e.value == '公司名'){
+            clickCompanyName(e)
+        }
+    })
     mychart.setOption(option);
 }
 
 
-// //点击跳转
-// mychart.on('click',function(e){
-//     // console.log(e.value); 
-//     //如果点击了企业法人，那么搜索该企业法人的数据并且返回新图谱
-//     if(e.value == '企业法人'){
-//         mychart.showLoading();
-//         console.log(e.name);
-//         $.get('http://106.14.151.119:3000/api/search?legalPerson='+e.name).done(function(data){
-//             console.log(e.name);
-//             mychart.hideLoading();
-//             // var mychart2 = echarts.init(document.getElementById('dataView'))
-//             var searchData = data;
-//             console.log(searchData);
-//             var dataArr = [{
-//                 name: e.name,
-//                 value:'企业法人',
-//                 itemStyle: {
-//                     normal: {
-//                         color:'darkblue'
-//                     }
-//                 },
-//                 symbolSize: 200,
-//             }];
-//             var linkArr = []
-            
 
-//             //遍历接收返回数据中的公司名
-//             for(var i = 1;i<searchData.length;i++){
-//                 console.log(searchData[i].name)
-//                 var companyObj = {
-//                     name: searchData[i].name,
-//                     value:'名下公司',
-//                     symbolSize: 100,
-//                 }
-//                 var linkObj = {
-//                     source: e.name,
-//                     target: searchData[i].name,       
-//                 }
-//                 dataArr.push(companyObj)
-//                 linkArr.push(linkObj)
-//             }
-//             console.log(dataArr)
-//             console.log(linkArr)
-
-
-//             mychart.setOption({
-//                 title: {
-//                     text: '企业法人'
-//                 },
-//                 tooltip: {},
-//                 animationDurationUpdate: 2500,
-//                 animationEasingUpdate: 'quinticInOut',
-//                 series : [
-//                     {
-//                         name:'企业法人',
-//                         type: 'graph',
-//                         layout: 'circular',
-//                         symbolSize: 90,
-//                         roam: true,
-//                         focusNodeAdjacency:true,
-//                         label: {
-//                             normal: {
-//                                 show: true,
-//                                 formatter:'{c}'       //标签内容格式器。模板变量有 {a}、{b}、{c}，分别表示系列名，数据名，数据值。
-//                             }
-//                         },
+//点击企业图谱后触发的图谱变化
+function clickLegalPerson(e){
+    mychart.showLoading();
+    console.log(e.name);
+    $.get('http://106.14.151.119:3000/api/search?legalPerson='+e.name).done(function(data){
+        console.log(e.name);
+        mychart.hideLoading();
+        var searchData = data;
+        console.log(searchData);
+        var dataArr = [{
+            name: e.name,
+            value:'企业法人',
+            itemStyle: {
+                normal: {
+                    color:'darkblue'
+                }
+            },
+            symbolSize: 150,
+        }];
+        var linkArr = []
         
-//                         edgeSymbol: ['circle', 'arrow'],
-//                         edgeSymbolSize: [1, 10],
-//                         edgeLabel: {
-//                             normal: {
-//                                 textStyle: {
-//                                     fontSize: 20
-//                                 }
-//                             }
-//                         },
-//                         itemStyle: {
-//                             normal: {
-//                                 color: 'lightblue'
-//                             }
-//                         },//圆圈样式
-//                         symbol:"circle", 
-//                         symbolSize: 100,//圈圈大小
+
+        //遍历接收返回数据中的公司名
+        for(var i = 1;i<searchData.length;i++){
+            console.log(searchData[i].name)
+            var companyObj = {
+                name: searchData[i].name,
+                value:'公司名',
+                symbolSize: 100,
+                itemStyle: {
+                    normal: {
+                        color:'darkblue'
+                    }
+                },
+            }
+            var linkObj = {
+                source: e.name,
+                target: searchData[i].name,       
+            }
+            dataArr.push(companyObj)
+            linkArr.push(linkObj)
+        }
+        console.log(dataArr)
+        console.log(linkArr)
 
 
-//                         data:dataArr,//数据
-//                         links: linkArr,//关系
+        mychart.setOption({
+            title: {
+                text: '企业法人'
+            },
+            tooltip:{
+                formatter:function(e){
+                    if(e.name == ''){
+                        return '暂无'
+                    }
+                    return e.value + ": " + e.name;
+                }
+            },
+            toolbox:{
+                show : true,
+                feature : {//启用功能
+                    restore : {show: true},//restore，还原，复位原始图表
+                    dataView: {show:true},//数据窗口
+                },
+                
+            },
+            animationDurationUpdate: 2500,
+            animationEasingUpdate: 'quinticInOut',
+            series : [
+                {
+                    name:'企业法人',
+                    type: 'graph',
+                    layout: 'circular',
+                    symbolSize: 90,
+                    roam: true,
+                    focusNodeAdjacency:true,
+                    label: {
+                        normal: {
+                            show: true,
+                            formatter:'{c}'       //标签内容格式器。模板变量有 {a}、{b}、{c}，分别表示系列名，数据名，数据值。
+                        }
+                    },
+    
+                    edgeSymbol: ['circle', 'arrow'],
+                    edgeSymbolSize: [1, 10],
+                    edgeLabel: {
+                        normal: {
+                            textStyle: {
+                                fontSize: 20
+                            }
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            color: 'lightblue'
+                        }
+                    },//圆圈样式
+                    symbol:"circle", 
+                    symbolSize: 100,//圈圈大小
 
 
-//                         lineStyle: {
-//                             normal: {
-//                                 opacity: 0.9,
-//                                 width: 2,
-//                                 curveness: 0
-//                             }
-//                         }
-//                     }
-//                 ]
-//             });
+                    data:dataArr,//数据
+                    links: linkArr,//关系
 
-//         })
-//     }
-//     //if点击了其他
-// })
 
+                    lineStyle: {
+                        normal: {
+                            opacity: 0.9,
+                            width: 2,
+                            curveness: 0
+                        }
+                    }
+                }
+            ]
+        });
+
+    })
+}
+//点击公司名后图谱发生的变化
+function clickCompanyName(e){
+    mychart.showLoading();
+    console.log(e.name);
+    $.get('http://106.14.151.119:3000/api/search?name='+e.name).done(function(data){
+        console.log("搜索："+e.name);
+        mychart.hideLoading();
+        var searchData = data[1];//获取以name准确查询到的公司数据
+        console.log(searchData);
+        var newOption = {
+            title: {
+                text: '企业信息图谱'
+            },
+            //悬浮标签显示的内容
+            tooltip:{
+                formatter:function(e){
+                    if(e.name == ''){
+                        return '暂无'
+                    }
+                    return e.value + ": " + e.name;
+                }
+            },
+            //工具箱
+            toolbox:{
+                show : true,
+                feature : {//启用功能
+                    restore : {show: true},//restore，还原，复位原始图表
+                    dataView: {show:true},//数据窗口
+                },
+                
+            },
+            animationDurationUpdate: 1500,
+            animationEasingUpdate: 'quinticInOut',
+            series : [
+                {
+                    type: 'graph',
+                    // layout: 'force',
+                    layout: 'circular',
+                    symbolSize: 90,
+                    roam: 'true',
+                    // force: {
+                    //     edgeLength: [100,200],//线的长度，这个距离也会受 repulsion，支持设置成数组表达边长的范围
+                    //     repulsion: 100//节点之间的斥力因子。值越大则斥力越大
+                    // },
+                    // draggable: true,//指示节点是否可以拖动
+                    label: {
+                        normal: {
+                            show: true,
+                            formatter:'{c}'       //标签内容格式器。模板变量有 {a}、{b}、{c}，分别表示系列名，数据名，数据值。
+                        }
+                    },
+    
+                    edgeSymbol: ['circle', 'arrow'],
+                    edgeSymbolSize: [1, 10],
+                    edgeLabel: {
+                        normal: {
+                            textStyle: {
+                                fontSize: 20
+                            }
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            color: 'lightblue'
+                        }
+                    },//圆圈样式
+                    symbol:"circle",       //图形形状 'circle', 'rect', 'roundRect', 'triangle', 'diamond', 'pin', 'arrow'
+                    symbolSize: 100,//圈圈大小
+    
+    
+                    data: [{
+                        name: searchData.name,
+                        value:'公司名',
+                        symbolSize: 100,
+                        itemStyle: {
+                            normal: {
+                                color:'darkblue'
+                            }
+                        }
+    
+                    }, {
+                        name: searchData.legalPerson,
+                        value:'企业法人',
+                        itemStyle: {
+                            normal: {
+                                color:'darkblue'
+                            }
+                        }
+                    }, {
+                        name: searchData.address,
+                        value:'公司地址',
+                        
+                    }, {
+                        name: searchData.registeredCapital,
+                        value:'注册资本',
+                        
+                    },{
+                        name: searchData.creditCode,
+                        value:'信用代码',
+                        
+                    },{
+                        name: searchData.setupTime,
+                        value:'成立时间',
+                        
+                    },{
+                        name: searchData.scope,
+                        value:'经营范围',
+                        
+                    },{
+                        name: searchData.province,
+                        value:'所属省份',
+                        
+                    },{
+                        name: searchData.mail,
+                        value:'邮箱',
+                        
+                    },{
+                        name: searchData.phone,
+                        value:'联系方式',
+                        
+                    },{
+                        name: searchData.morePhone,
+                        value:'更多联系方式',
+                        
+                    }],
+                    // links: [],
+                    links: [{
+                        source: searchData.legalPerson,
+                        target: searchData.name,       
+                    }, {
+                        source: searchData.address,
+                        target: searchData.name,
+                    }, {
+                        source: searchData.setupTime,
+                        target: searchData.name
+                    }, {
+                        source: searchData.phone,
+                        target: searchData.name
+                    }, {
+                        source:  searchData.scope,
+                        target: searchData.name
+                    }, {
+                        source: searchData.province,
+                        target: searchData.name
+                    },{
+                        source: searchData.morePhone,
+                        target: searchData.name
+                    },{
+                        source: searchData.registeredCapital,
+                        target: searchData.name
+                    },{
+                        source: searchData.mail,
+                        target: searchData.name
+                    }],
+                    lineStyle: {
+                        normal: {
+                            opacity: 0.9,
+                            width: 2,
+                            curveness: 0
+                        }
+                    }
+                }
+            ]
+        }
+        mychart.setOption(newOption);
+    })
+
+    
+}
--- companyQuery/js/searchResult.js
+++ companyQuery/js/searchResult.js
@@ -1,100 +1,75 @@
 var companyObj = new Object();//该对象存储name和id关键信息
 var resultNumber;//发送请求返回的数据条数
 var resultData;//请求到的数据
+var paramType,
+    paramName,
+    paramAddress,
+    paramPhone,
+    paramLegalPerson,
+    paramCreditCode,
+    paramScope;
+function requestParam(paramType,param,pageIndex){
+
+    var url = "http://106.14.151.119:3000/api/search?"+paramType+"="+param+"&page="+pageIndex;
+    console.log(url);
+    $.get(url,function(data,status){
+        resultData = data;
+        console.log(resultData);
+        resultNumber = data[0]['数量'];//返回的结果数量
+        $('#resultNumber').text(resultNumber);
+        createList(resultNumber);//动态创建列表
+        addDataToList(resultData);//动态加载数据  
+    })
+}
+
 $(function(){
     //按企业名称搜索
     if($.query.get("name")!=''){
-        //获取传递过来的name值
-        var name = $.query.get("name");
-        console.log("获取到的数据："+ name);
-        //发送ajax请求申请数据
-        var url = "http://106.14.151.119:3000/api/search?name="+name;
-        $.get(url,function(data,status){
-            resultData = data;
-            console.log(resultData);
-            resultNumber = data[0]['数量'];//返回的结果数量
-            $('#resultNumber').text(resultNumber);
-            createList(resultNumber);//动态创建列表
-            addDataToList(resultData);//动态加载数据  
-        })
+        paramType = 'name';
+        paramName = $.query.get("name");
+        console.log("获取到的数据："+ paramName);
+        requestParam(paramType,paramName,1);
+
+
     }
     //按地址搜索
     if($.query.get("address")!=''){
-        var address = $.query.get("address");
-        console.log("获取到的数据："+ address);
-        var url = "http://106.14.151.119:3000/api/search?address="+address;
-        $.get(url,function(data,status){
-            resultData = data;
-            console.log(resultData);
-            resultNumber = data[0]['数量'];//返回的结果数量
-            $('#resultNumber').text(resultNumber);
-            createList(resultNumber);//动态创建列表
-            addDataToList(resultData);//动态加载数据  
-        })
+        paramType = 'address';
+        paramAddress = $.query.get("address");
+        console.log("获取到的数据："+ paramAddress);
+        requestParam(paramType,paramAddress,1);
     }
     //按联系方式搜索
     if($.query.get("phone")!=''){
         //获取传递过来的phone值
-        var phone = $.query.get("phone");
-        console.log("获取到的数据："+ phone);
-        //发送ajax请求申请数据
-        var url = "http://106.14.151.119:3000/api/search?phone="+phone;
-        $.get(url,function(data,status){
-            resultData = data;
-            console.log(resultData);
-            resultNumber = data[0]['数量'];//返回的结果数量
-            $('#resultNumber').text(resultNumber);
-            createList(resultNumber);//动态创建列表
-            addDataToList(resultData);//动态加载数据  
-        })
+        paramType = 'phone';
+        paramPhone= $.query.get("phone");
+        console.log("获取到的数据："+ paramPhone);
+        requestParam(paramType,paramPhone,1);
+        
     }
     //按法人股东名搜索
     if($.query.get("legalPerson")!=''){
-        //获取传递过来的legalPerson值
-        var legalPerson = $.query.get("legalPerson");
-        console.log("获取到的数据："+ legalPerson);
-        //发送ajax请求申请数据
-        var url = "http://106.14.151.119:3000/api/search?legalPerson="+legalPerson;
-        $.get(url,function(data,status){
-            resultData = data;
-            console.log(resultData);
-            resultNumber = data[0]['数量'];//返回的结果数量
-            $('#resultNumber').text(resultNumber);
-            createList(resultNumber);//动态创建列表
-            addDataToList(resultData);//动态加载数据  
-        })
+        paramType = 'legalPerson';
+        paramLegalPerson = $.query.get("legalPerson");
+        console.log("获取到的数据："+ paramLegalPerson);
+        requestParam(paramType,paramLegalPerson,1);
+        
     }
     //按统一社会信用代码搜索
     if($.query.get("creditCode")!=''){
-        //获取传递过来的creditCode值
-        var creditCode = $.query.get("creditCode");
-        console.log("获取到的数据："+ creditCode);
-        //发送ajax请求申请数据
-        var url = "http://106.14.151.119:3000/api/search?creditCode="+creditCode;
-        $.get(url,function(data,status){
-            resultData = data;
-            console.log(resultData);
-            resultNumber = data[0]['数量'];//返回的结果数量
-            $('#resultNumber').text(resultNumber);
-            createList(resultNumber);//动态创建列表
-            addDataToList(resultData);//动态加载数据  
-        })
+        paramType = 'creditCode';
+        paramCreditCode = $.query.get("creditCode");
+        console.log("获取到的数据："+ paramCreditCode);
+        requestParam(paramType,paramCreditCode,1);
     }
     //按行业搜索
     if($.query.get("scope")!=''){
         //获取传递过来的name值
-        var scope = $.query.get("name");
-        console.log("获取到的数据："+ scope);
-        //发送ajax请求申请数据
-        var url = "http://106.14.151.119:3000/api/search?scope="+scope;
-        $.get(url,function(data,status){
-            resultData = data;
-            console.log(resultData);
-            resultNumber = data[0]['数量'];//返回的结果数量
-            $('#resultNumber').text(resultNumber);
-            createList(resultNumber);//动态创建列表
-            addDataToList(resultData);//动态加载数据  
-        })
+        paramType = 'scope';
+        paramScope = $.query.get("scope");
+        console.log("获取到的数据："+ paramScope);
+        requestParam(paramType,paramScope,1);
     }
     
 });
@@ -106,7 +81,7 @@ function createList(number){
         
     }
     //生成列表
-    for(var i = 0;i<number;i++){
+    for(var i = 0;i<10;i++){
         listDOM();
     }
 }
